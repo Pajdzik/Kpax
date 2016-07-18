3d90986ef761644126a64bb42652ef161f84bf51
Kamil Pajdzik <pajdziupaj@gmail.comn>
2016-07-17 20:30:06 -0700

    Add Verify class

---
+++ Source/Eks.Core.UnitTests/Verification/VerifyTests.cs
@@ -0,0 +1,72 @@
+﻿using FluentAssertions;
+using System;
+using Eks.Core.Verification;
+using Xunit;
+
+namespace Eks.Core.UnitTests.Verification
+{
+    public class VerifyTests
+    {
+        public class IsNullTests
+        {
+            [Fact]
+            public void IsNull_ExceptionThrown()
+            {
+                Action action = () => Verify.IsNull(null);
+                action.ShouldThrow<ArgumentNullException>().And.ParamName.Should().BeNull();
+            }
+
+            [Fact]
+            public void SimpleType_NoExceptionThrown()
+            {
+                Action action = () => Verify.IsNull(0);
+                action.ShouldNotThrow<ArgumentNullException>();
+            }
+
+            [Fact]
+            public void ObjectType_NoExceptionThrown()
+            {
+                Action action = () => Verify.IsNull(new object());
+                action.ShouldNotThrow<ArgumentNullException>();
+            }
+        }
+
+        public class IsNullParameterNameTests
+        {
+            [Fact]
+            public void IsNull_ExceptionThrown()
+            {
+                object parameter = null;
+                Action action = () => Verify.IsNull(parameter, nameof(parameter));
+                action.ShouldThrow<ArgumentNullException>().And.ParamName.Should().Be(nameof(parameter));
+            }
+
+            [Fact]
+            public void SimpleType_NoExceptionThrown()
+            {
+                var parameter = 0;
+                Action action = () => Verify.IsNull(parameter, nameof(parameter));
+                action.ShouldNotThrow<ArgumentNullException>();
+            }
+
+            [Fact]
+            public void ObjectType_NoExceptionThrown()
+            {
+                var parameter = new object();
+                Action action = () => Verify.IsNull(parameter, nameof(parameter));
+                action.ShouldNotThrow<ArgumentNullException>();
+            }
+        }
+
+        public class IsNullParameterNamMessageeTests
+        {
+            [Fact]
+            public void IsNull_ExceptionThrown()
+            {
+                object parameter = null;
+                Action action = () => Verify.IsNull(parameter, nameof(parameter), "Parameter should not be null");
+                action.ShouldThrow<ArgumentNullException>().And.Message.Should().Be("Parameter should not be null\r\nParameter name: parameter");
+            }
+        }
+    }
+}
---
+++ Source/Eks.Core/Verification/Verify.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eks.Core.Verification
+{
+    public static class Verify
+    {
+        public static void IsNull(object o, string parameterName = null, string message = null)
+        {
+            if (o == null)
+            {
+                throw new ArgumentNullException(parameterName, message);
+            }
+        }
+    }
+}
