a4e9839649bee8eadfe8ffb5e33b40da0c29ddd8
Kamil Pajdzik <pajdziupaj@gmail.comn>
2016-08-09 22:28:39 -0700

    Add parameterless GetNumberDigitCount method

--- Source/Eks.Core/Math/RoundingExtensions.cs
+++ Source/Eks.Core/Math/RoundingExtensions.cs
@@ -4,32 +4,49 @@ namespace Eks.Core.Maths
 {
     public static class RoundingExtensions
     {
-        public static int RoundToSignificantDigits(this int digit, int significantDigits)
+        public static int RoundToSignificantDigits(this int number)
         {
-            return (int)RoundingExtensions.RoundToSignificantDigits((double)digit, significantDigits);
+            var digits = GetNumberDigitCount(number)/3;
+            return (int)RoundingExtensions.RoundToSignificantDigits((double)number, digits);
         }
 
-        public static long RoundToSignificantDigits(this long digit, int significantDigits)
+
+        public static int RoundToSignificantDigits(this int number, int significantDigits)
         {
-            return (long) RoundingExtensions.RoundToSignificantDigits((double) digit, significantDigits);
+            return (int)RoundingExtensions.RoundToSignificantDigits((double)number, significantDigits);
         }
 
-        public static double RoundToSignificantDigits(this double digit, int significantDigits)
+        public static long RoundToSignificantDigits(this long number, int significantDigits)
         {
-            return digit.RoundToSignificantDigits(significantDigits, 0.01);
+            return (long) RoundingExtensions.RoundToSignificantDigits((double) number, significantDigits);
         }
 
-        public static double RoundToSignificantDigits(this double digit, int significantDigits, double delta)
+        public static double RoundToSignificantDigits(this double number, int significantDigits)
         {
-            if (Math.Abs(digit) < delta)
+            return number.RoundToSignificantDigits(significantDigits, 0.01);
+        }
+
+        public static double RoundToSignificantDigits(this double number, int significantDigits, double delta)
+        {
+            if (Math.Abs(number) < delta)
             {
                 return 0;
             }
 
-            var numberOfDigits = Math.Floor(Math.Log10(Math.Abs(digit))) + 1;
+            var numberOfDigits = Math.Floor(Math.Log10(Math.Abs(number))) + 1;
             var scale = Math.Pow(10, numberOfDigits);
 
-            return scale*Math.Round(digit/scale, significantDigits);
+            return scale*Math.Round(number/scale, significantDigits);
+        }
+
+        private static int GetNumberDigitCount(int number)
+        {
+            if (number == 0)
+            {
+                return 1;
+            }
+
+            return (int) (Math.Floor(Math.Log10(Math.Abs(number))) + 1);
         }
     }
 }
