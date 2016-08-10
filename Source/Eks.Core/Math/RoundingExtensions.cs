c5959a750b42f13b60414c36848d6c224549c871
Kamil Pajdzik <pajdziupaj@gmail.comn>
2016-08-09 21:13:23 -0700

    Add RoundingExtensions

---
+++ Source/Eks.Core/Math/RoundingExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Eks.Core.Maths
+{
+    public static class RoundingExtensions
+    {
+        public static int RoundToSignificantDigits(this int digit, int significantDigits)
+        {
+            return (int)RoundingExtensions.RoundToSignificantDigits((double)digit, significantDigits);
+        }
+
+        public static long RoundToSignificantDigits(this long digit, int significantDigits)
+        {
+            return (long) RoundingExtensions.RoundToSignificantDigits((double) digit, significantDigits);
+        }
+
+        public static double RoundToSignificantDigits(this double digit, int significantDigits)
+        {
+            return digit.RoundToSignificantDigits(significantDigits, 0.01);
+        }
+
+        public static double RoundToSignificantDigits(this double digit, int significantDigits, double delta)
+        {
+            if (Math.Abs(digit) < delta)
+            {
+                return 0;
+            }
+
+            var numberOfDigits = Math.Floor(Math.Log10(Math.Abs(digit))) + 1;
+            var scale = Math.Pow(10, numberOfDigits);
+
+            return scale*Math.Round(digit/scale, significantDigits);
+        }
+    }
+}
