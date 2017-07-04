44602909763badde301b6931508d9345be5b9aab
Kamil Pajdzik <pajdziupaj@gmail.com>
2017-07-03 21:09:37 -0700

    Add Eks.Abstraction project

---
+++ Source/Eks.Abstraction/System.IO/DirectoryProxy.cs
@@ -0,0 +1,17 @@
+﻿namespace Eks.Abstraction.System.IO
+{
+    using global::System.IO;
+
+    public class DirectoryProxy : IDirectoryProxy
+    {
+        public bool Exists(string path)
+        {
+            return Directory.Exists(path);
+        }
+
+        public string[] GetFiles(string path)
+        {
+            return Directory.GetFiles(path);
+        }
+    }
+}
---
+++ Source/Eks.Abstraction/System.IO/IDirectoryProxy.cs
@@ -0,0 +1,9 @@
+﻿namespace Eks.Abstraction.System.IO
+{
+    public interface IDirectoryProxy
+    {
+        bool Exists(string path);
+
+        string[] GetFiles(string path);
+    }
+}
